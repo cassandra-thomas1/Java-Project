a255fae7600eeb1b5f9388b11858ee2b31bc724b
jnewbury4 <joseph.s.newbury@gmail.com>
2020-04-28 21:11:54 -0400

    haha

--- src/app/FrontEnd.java
+++ src/app/FrontEnd.java
@@ -1,4 +1,14 @@
 package app;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class FrontEnd {
+public class FrontEnd extends Application{
+    @Override
+    public void start(Stage stage) throws FileNotFoundException{
+        Image test = new Image(new FileInputStream("C:\\Users\\Joey\\IdeaProjects\\Java-Project\\kenyualtother.png"));
+    }
 }
