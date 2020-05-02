d4b7dc32e6fa1cff02fb72b72bd80cf9c1c30973
jnewbury4 <joseph.s.newbury@gmail.com>
2020-05-01 22:07:58 -0400

    haha

--- src/app/FrontEnd.java
+++ src/app/FrontEnd.java
@@ -1,14 +1,40 @@
 package app;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class FrontEnd extends Application{
     @Override
-    public void start(Stage stage) throws FileNotFoundException{
-        Image test = new Image(new FileInputStream("C:\\Users\\Joey\\IdeaProjects\\Java-Project\\kenyualtother.png"));
+    public void start(Stage primaryStage) throws IOException {
+        Group root = new Group();
+        Canvas canvas = new Canvas (1280, 720);
+        root.getChildren().add(canvas);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //loading asset
+        Image test = new Image(new URL("https://wizardlyshoe4.s-ul.eu/3QeXMcHs").openStream());
+        gc.drawImage(test, 0, 0);
+
+        //Create and display scene
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
