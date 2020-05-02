bccf2899b9740db762ea8e05cdd217fa2b9eeb09
jnewbury4 <joseph.s.newbury@gmail.com>
2020-05-02 00:25:39 -0400

    the stuff is only displayed for testing purposes right now

--- src/app/FrontEnd.java
+++ src/app/FrontEnd.java
@@ -1,16 +1,17 @@
 package app;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 
@@ -18,18 +19,45 @@ public class FrontEnd extends Application{
     @Override
     public void start(Stage primaryStage) throws IOException {
         Group root = new Group();
-        Canvas canvas = new Canvas (1280, 720);
+        Canvas canvas = new Canvas (540, 960);
         root.getChildren().add(canvas);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        //loading assets
-        Image test = new Image(new URL("https://wizardlyshoe4.s-ul.eu/3QeXMcHs").openStream());
-        gc.drawImage(test, 0, 0);
-
         //Create and display scene
         Scene scene = new Scene(root);
         primaryStage.setTitle("Game");
         primaryStage.setScene(scene);
+
+        ArrayList<String> input = new ArrayList<>();
+
+        scene.setOnKeyPressed(e -> { //obtain input from user
+                    String code = e.getCode().toString();
+                    if ( !input.contains(code) )
+                        input.add(code);
+                });
+        scene.setOnKeyReleased(e -> { //clear the input when key is not being pressed
+                    String code = e.getCode().toString();
+                    input.remove( code );
+                });
+
+        //loading assets
+        Image ship = new Image(new URL("https://wizardlyshoe4.s-ul.eu/JavaProject/PcY9pXSJ").openStream());
+        Image enemyShip = new Image(new URL("https://wizardlyshoe4.s-ul.eu/JavaProject/6ayA0vm0").openStream());
+        Image backGround = new Image(new URL("https://wizardlyshoe4.s-ul.eu/JavaProject/Njqkixku").openStream());
+
+        final long startNanoTime = System.nanoTime();
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                gc.drawImage(backGround,0,0);
+                gc.drawImage(ship, 240, 850);
+                gc.drawImage(enemyShip, 240, 0);
+            }
+        }.start();
+
+
         primaryStage.show();
     }
 
