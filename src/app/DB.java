ae4eacb5f5c8a77d81ff37a8eebb25c081915539
Cassandra Thomas <43892548+cassandra-thomas@users.noreply.github.com>
2020-05-11 02:17:14 -0400

    .

--- src/app/DB.java
+++ src/app/DB.java
@@ -1,27 +1,33 @@
 package app;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DB {
     private static Connection gameConn;
-
-    static void access() throws SQLException {
+    private static ArrayList<Score> allScores = new ArrayList<Score>();
+    static ArrayList<Score> access() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/appdata";
         String user = "root";
         String password = "Password";
         try{
+            //access the DB using parameters above
             gameConn = DriverManager.getConnection(url, user, password);
             Statement myStatement = gameConn.createStatement();
             ResultSet returned = myStatement.executeQuery("select * from appdata.scores");
 
+            //iterate the DB file and creates new score instances
             while (returned.next()){
-               //make new scores
+               allScores.add(new Score(returned.getString("username"), returned.getInt("score"),
+                       returned.getInt("timealive"), returned.getInt("shipsdestroyed")));
             }
 
         }catch (SQLException ex){
             ex.printStackTrace();
         }
 
+
+        return allScores;
     }
 
     static void close() throws SQLException {
