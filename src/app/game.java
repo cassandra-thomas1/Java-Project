d65a2a6f315e52e2b6fd34f8a88066d6db9a72a1
Cassandra Thomas <43892548+cassandra-thomas@users.noreply.github.com>
2020-05-12 17:20:33 -0400

    .

--- src/app/game.java
+++ src/app/game.java
@@ -14,9 +14,9 @@ import java.util.Random;
 //this needs to be commented too
 public class game {
 
-    private static ArrayList<Enemy> enemyList = new ArrayList<Enemy>();
+    private static ArrayList<Enemy> enemyList = new ArrayList<Enemy>();     //containers that hold the lasers and enemy ships
     private static ArrayList<Laser> lasers = new ArrayList<Laser>();
-    private static String[] backgrounds = new String[] {"Resources\\BlueSpace.png", "Resources\\ResSpace.png", "Resources\\PinkSpace.png"};
+    private static String[] backgrounds = new String[] {"Resources\\BlueSpace.png", "Resources\\ResSpace.png", "Resources\\PinkSpace.png"};     //container that holds the scrolling background
     private static Sprite playerShip;
     private static Background space;
     private static Random rand = new Random();
@@ -26,6 +26,8 @@ public class game {
     private static Timer dispTimer;
     private static int shipsKilled = 0;
 
+    //pre: game is started
+    //post: game is initialized with all the properties of the game
     public static void startLevel() throws IOException {
         playerShip = new Player(240, 850, 72, 64, "Resources\\PlayerShip.png");
         Enemy enemyShip = new Enemy(240, 0, 72, 64, "Resources\\EnemyShip.png");
@@ -35,12 +37,21 @@ public class game {
         space = new Background(backgrounds[rand.nextInt(3)], 5056);
     }
 
-
+    //pre: the game is running at 60 frames/second
+    //post: handles game logic such as movement, collisions and score accumulation
     static void logic(GraphicsContext gc, long frames, ArrayList<String> input) throws IOException, ConcurrentModificationException {
+        //the background continuously is moving and
+        //being rendered to place an effect of moving through space
+        space.scroll();
+        space.render(gc);
+
+        //player ship moves based on key movements
         if (input.contains("LEFT") && playerShip.getX() > 0)
             playerShip.move(-3);
         if (input.contains("RIGHT") && playerShip.getX() < 468)
             playerShip.move(3);
+
+        //every second, player shoots a laser, and the timer is manually updated
         if (frames % 60 == 0) {
             Laser laser = new Laser(playerShip.getX() + 33, playerShip.getY() - 12, 4, 6, "Resources\\bullet.png");
             lasers.add(laser);
@@ -51,33 +62,39 @@ public class game {
         }
 
 
-
+        //every 1.5 seconds, an enemy is spawned from the top of the screen
         if (frames % 90 == 0) {
             Enemy enemyShip = new Enemy(rand.nextInt(540 - 72), 0 - 64, 72, 64, "Resources\\EnemyShip.png");
             enemyList.add(enemyShip);
         }
 
+
+        //every 2 seconds, the player gets points for being alive
         if(frames % 120 == 0){
             score += 10;
             dispScore.updateText(score);
         }
 
 
-        space.scroll();
-        space.render(gc);
+        //both loops below handle the movement and
+        // collision status of the enemy ships and lasers
         for (Enemy enemyShip : enemyList ) {
             enemyShip.move(1);
             enemyShip.render(gc);
             if (enemyShip.intersects(playerShip) || enemyShip.getY() > (playerShip.getY() + playerShip.getHeight())) {
                 enemyShip.kill();
-                playerShip.kill();
+                playerShip.kill();      //the game will end if the player dies
             }
         }
+
         for (Laser laser : lasers){
+            //lasers only exist in the game bounds
             if(laser.getY() >= 0) {
                 laser.move(5);
                 laser.render(gc);
                 for (Enemy enemyShip : enemyList ){
+                    //when an enemy is hit, points are added to the score
+                    //and the enemy and laser no longer exist
                     if(enemyShip.intersects(laser)){
                         enemyShip.kill();
                         laser.kill();
@@ -89,15 +106,21 @@ public class game {
                 }
             }
             else{
-                laser.kill();
+                laser.kill();       //the laser has passed y = 0
             }
         }
+
+        //displays singular assets of the game
         playerShip.render(gc);
         dispScore.render(gc);
         dispTimer.render(gc);
 
+
+
     }
 
+    //pre: frontend needs to check if the game is still in session
+    //post: If the player is still alive, return it's status(true), else end the level and return it's status(false)
     static boolean running(){
         if(!playerShip.isAlive()){
             levelEnd();
@@ -105,14 +128,16 @@ public class game {
         return playerShip.isAlive();
     }
 
+    //pre: the game properties are needed
+    //post: returns the necessary value
     static long getScore(){
         return score;
     }
-
     static long getTime() {return time;}
-
     static int getShipsKilled(){ return shipsKilled;}
 
+    //pre: the player ship has been killed
+    //post: clears the containers holding the lasers and ships
     static void levelEnd(){
         enemyList.clear();
         lasers.clear();
