ed6b3dfe1173cae3c68da5de43ba2dc315475b09
jnewbury4 <joseph.s.newbury@gmail.com>
2020-05-12 18:51:13 -0400

    haha

--- src/app/FrontEnd.java
+++ src/app/FrontEnd.java
@@ -23,7 +23,6 @@ import javafx.scene.layout.Background;
 import javafx.stage.Stage;
 
 //this class will call all the methods necessary to make the app run
-//hhhahahha
 public class FrontEnd extends Application {
     private TableView<Score> table = new TableView<>();
     private String playerName;
@@ -33,10 +32,9 @@ public class FrontEnd extends Application {
     public void start(Stage primaryStage) throws IOException, SQLException {
         //load in database
         ArrayList<Score> scores = DB.access();
-
-        primaryStage.setTitle("Space Shooter");
-        //main menu button actions
+        //initialize the text field for player name entry
         TextField nameEntry = new TextField();
+        //event handler for the start game button
         EventHandler<ActionEvent> startGame = e -> {
             gameStart = true;
             playerName = nameEntry.getText();
@@ -56,12 +54,15 @@ public class FrontEnd extends Application {
         ImageView imageView = new ImageView(new Image(new FileInputStream("Resources\\background.png")));
         ImageView imageView2 = new ImageView(new Image(new FileInputStream("Resources\\background.png")));
 
+        //main menu buttons and labels
+        primaryStage.setTitle("Space Shooter");
         Label namePls = new Label("Name: ");
         namePls.setStyle("-fx-text-fill: #00ffbd; -fx-font-weight: bold; -fx-font-size: 20");
         Button toGame = new Button("Start game");
         Button toScore = new Button("High scores");
         toGame.setOnAction(startGame);
         toScore.setOnAction(e->primaryStage.setScene(scoreScene));
+        //main menu vbox to contain all of the elements created above
         VBox menuOptions = new VBox();
         menuOptions.getChildren().addAll(namePls, nameEntry, toGame, toScore);
         menuOptions.setMaxWidth(200);
@@ -69,7 +70,7 @@ public class FrontEnd extends Application {
         menuOptions.setSpacing(10);
         StackPane menuBox = new StackPane();
         StackPane.setAlignment(menuBox, Pos.CENTER);
-        menuBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
+        menuBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");//transparency property so that we can see the background
         menuBox.getChildren().addAll(imageView, menuOptions);
         menuScene = new Scene(menuBox, 540, 960);
 
@@ -80,14 +81,12 @@ public class FrontEnd extends Application {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gameScene = new Scene(gamePane);
 
-        //score screen
-        //labels, buttons, and event handlers
+        //score screen buttons, labels, and event handlers
         Label scoreMenu = new Label("High Scores");
         scoreMenu.setStyle("-fx-text-fill: #00ffbd; -fx-font-weight: bold; -fx-font-size: 20");
         Button toMenu = new Button("Main Menu");
         toMenu.setOnAction(e->primaryStage.setScene(menuScene));
-        //create table
-
+        //create table and columns to display high scores
         Label playerColumn = new Label("Player");
         playerColumn.setMinWidth(100);
         playerColumn.setStyle("-fx-font-weight: bold");
@@ -101,8 +100,7 @@ public class FrontEnd extends Application {
         shipsKilledColumn.setMinWidth(100);
         shipsKilledColumn.setStyle("-fx-font-weight: bold");
 
-
-        //create and populate score vbox
+        //create and populate score vbox to contain score properties for the scoreboard
         VBox scoreOptions = new VBox();
         scoreOptions.getChildren().addAll(scoreMenu, new HBox(playerColumn, scoreColumn, timeColumn, shipsKilledColumn));
         scoreOptions.setMaxWidth(400);
@@ -110,10 +108,10 @@ public class FrontEnd extends Application {
         scoreOptions.setBackground(Background.EMPTY);
         scoreOptions.setStyle("-fx-background-color: rgba(255, 255, 255, 0.5);");
 
-
+        //loop to populate the scoreboard with high scores
         int count = 0;      //counter for the loop
         Collections.sort(scores);
-        //adds top 5 scores to the scoreboard
+        //populate the score board with top 5 scores
         for(Score s : scores){
             Label user = new Label(s.getPlayerName());
             Label score = new Label(Long.toString(s.getScore()));
@@ -129,20 +127,20 @@ public class FrontEnd extends Application {
                 count++;
             }
         }
-
+        //add menu button
         scoreOptions.getChildren().add(toMenu);
 
         //create and populate stackpane
         StackPane scoreBox = new StackPane();
         StackPane.setAlignment(scoreBox, Pos.CENTER);
-        scoreBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
+        scoreBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");//transparency so we can see the background
         scoreBox.getChildren().addAll(imageView2, scoreOptions);
+
         //set scene
-
         scoreScene = new Scene(scoreBox, 540, 960);
-
         primaryStage.setScene(menuScene);
 
+        //these are the event handlers for obtaining input from the user during the game
         ArrayList<String> input = new ArrayList<>();
         gameScene.setOnKeyPressed(e -> { //obtain input from user
             String code = e.getCode().toString();
@@ -153,23 +151,25 @@ public class FrontEnd extends Application {
             String code = e.getCode().toString();
             input.remove(code);
         });
-        final long startTime = System.nanoTime();
-        new AnimationTimer()//game time
+
+        final long startTime = System.nanoTime();// this is the time that the program originally starts at from the system which we can use to calculate elapsed time
+        //animation timer gets called every frame that the program is running
+        new AnimationTimer()
         {
+            //handle is called by AnimationTimer and is passed the time in nanoseconds from the system every frame
             public void handle(long currentTime) {
-                long elapsedTime = currentTime - startTime;
-                int elapsedFrames = Math.toIntExact(elapsedTime / 16_670_000);
+                long elapsedTime = currentTime - startTime;//we use this to calculate the elapsed time
+                int elapsedFrames = Math.toIntExact(elapsedTime / 16_670_000); //convert elapsed time into 60 frames per second to pass to the game logic
                 long lastUpdate = 0;
-                if (currentTime - lastUpdate >= 16_670_000 && gameStart) {
+                if (currentTime - lastUpdate >= 16_670_000 && gameStart) {//only send updates to the game logic ever 60th of a second if gamestart is true
                     try {
                         game.logic(gc, elapsedFrames, input);
-                        if (!game.running()){
+                        if (!game.running()){ //if the game is not running (that function gets set to false when the player dies)
                             gameStart = false;
-                            try {
+                            try {// add a new score object to the database
                                 DB.add(new Score(playerName, game.getScore(), game.getTime(), game.getShipsKilled()));
                             }catch (SQLException e){}
-
-                            primaryStage.setScene(scoreScene);
+                            primaryStage.setScene(scoreScene);//go to the score screen
                         }
                     } catch (IOException | ConcurrentModificationException e) {
                         e.printStackTrace();
@@ -179,6 +179,7 @@ public class FrontEnd extends Application {
         }.start();
         primaryStage.show();
     }
+    //main method
     public static void main(String[] args) throws SQLException {
         launch(args);
         DB.close();
--- src/app/Score.java
+++ src/app/Score.java
@@ -1,46 +1,60 @@
 package app;
 
-public class Score implements Comparable<Score>{
+public class Score implements Comparable<Score>{//implements Comparable so that we can use Collections.sort on the player class
     private String playerName;
     private long score;
     private long time;
     private long shipsKilled;
-    public Score(){
-        playerName = "";
-        score = 0;
-        time = 0;
-        shipsKilled = 0;
-    }
+    //constructor initializes the a score object which contains the player's name, score, time played, and ships killed
     public Score(String playerName, long score, long time, long shipsKilled){
         setPlayerName(playerName);
         setScore(score);
         setTime(time);
         setShipsKilled(shipsKilled);
     }
+    //precondition: none
+    //postcondition: sets the player name
     public void setPlayerName(String playerName){
         this.playerName = playerName;
     }
+    //precondition: none
+    //postcondition: sets the player score
     public void setScore(long score){
         this.score = score;
     }
+    //precondition: none
+    //postcondition: sets the player time played
     public void setTime(long time){
         this.time = time;
     }
+    //precondition: none
+    //postcondition: sets the player number of ships killed
     public void setShipsKilled(long shipsKilled){
         this.shipsKilled = shipsKilled;
     }
+    //precondition: none
+    //postcondition: returns the player number of ships killed
     public String getPlayerName(){
         return playerName;
     }
+    //precondition: none
+    //postcondition: returns the player number of ships killed
     public long getScore(){ return score; }
+    //precondition: none
+    //postcondition: returns the player number of ships killed
     public long getTime(){
         return time;
     }
+    //precondition: none
+    //postcondition: returns the player number of ships killed
     public long getShipsKilled(){
         return shipsKilled;
     }
 
     @Override
+    //precondition: none
+    //postcondition: returns a negative integer if the passed in score is less than this score
+    //  positive integer if the passed in score is greater, 0 if they are the same
     public int compareTo(Score scoreIn) {
         return Math.toIntExact(scoreIn.getScore() - score);
     }
